b03afa1b127adc92e51e2348d8a7faba4b56d875
ahad-salim <ahadsalim342gmail.com>
2026-09-23 13:00:07 +0100

    cretaed the response formater

---
+++ src/utils/apiResponces.js
@@ -0,0 +1,18 @@
+const successResponse = (res, message, data = {}, statusCode = 200) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+const errorResponse = (res, message, errors = [], statusCode = 400) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors,
+  });
+};
+
+
+export { successResponse, errorResponse };
